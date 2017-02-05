d3f9472be7d9a7f6ba426e5b68e0a70c48001a08
jackk <jackk@192.168.0.178>
2017-02-05 21:46:10 +0000

    Add food form work

--- src/main/webapp/src/views/food/add-food/add-food.component.ts
+++ src/main/webapp/src/views/food/add-food/add-food.component.ts
@@ -8,17 +8,23 @@ import * as $ from "jquery";
     templateUrl: 'add-food.template.html'
 })
 export class AddFoodComponent implements OnInit {
-    constructor() { }
+    
+    constructor() {
+    }
 
     ngOnInit() {
-
         console.log("This bullshit is now working")
-
-        $(".touchspin1").TouchSpin({
-            verticalbuttons: true,
-            buttondown_class: 'btn btn-white',
-            buttonup_class: 'btn btn-white'
-        });
-
+        $(".touchspin3").TouchSpin(
+            data
+        );
     }
 }
+
+var data = {
+    min: 0,
+    max: 5000,
+    step: 1,
+    decimals: 2,
+    buttondown_class: 'btn btn-white',
+    buttonup_class: 'btn btn-white'
+};
