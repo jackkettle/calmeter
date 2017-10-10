396cf50e015a70ab9399f64bfa7f17be594d8bb2
jackk <jackk@192.168.0.178>
2017-10-10 22:39:27 +0100

    Added TODO: Implement JWT verify method

--- src/main/java/com/calmeter/core/security/auth/jwt/verifier/JwtTokenVerifier.java
+++ src/main/java/com/calmeter/core/security/auth/jwt/verifier/JwtTokenVerifier.java
@@ -6,6 +6,17 @@ import org.springframework.stereotype.Component;
 public class JwtTokenVerifier implements TokenVerifier {
     @Override
     public boolean verify(String jti) {
+        
+        // TODO: Implement JWT verify method
+        
+        // well formed jwt
+        
+        // check the signature
+        
+        // validate the standard claims
+        
+        // check the client permissions
+        
         return true;
     }
 }
