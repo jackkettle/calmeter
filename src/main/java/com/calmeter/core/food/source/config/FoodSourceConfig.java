9d89e6ad4ca52b3b793a9b75d7b90338f90488eb
Jack Kettle <jack.kettle2@mail.dcu.ie>
2017-12-02 22:20:34 +0000

    Converting to Gradle

--- src/main/java/com/calmeter/core/food/source/config/FoodSourceConfig.java
+++ src/main/java/com/calmeter/core/food/source/config/FoodSourceConfig.java
@@ -1,21 +1,22 @@
 package com.calmeter.core.food.source.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-@Component
-@ConfigurationProperties(locations = "classpath:/food-source.properties", prefix="food.source")
+@Configuration
+@PropertySource("classpath:food-source.properties")
+@ConfigurationProperties(prefix = "food.source")
 public class FoodSourceConfig {
 
-	private String tescoSubscriptionKey;
+    private String tescoSubscriptionKey;
 
-	
-	public String getTescoSubscriptionKey () {
-		return tescoSubscriptionKey;
-	}
+    public String getTescoSubscriptionKey() {
+        return tescoSubscriptionKey;
+    }
 
-	public void setTescoSubscriptionKey (String tescoSubscriptionKey) {
-		this.tescoSubscriptionKey = tescoSubscriptionKey;
-	}
+    public void setTescoSubscriptionKey(String tescoSubscriptionKey) {
+        this.tescoSubscriptionKey = tescoSubscriptionKey;
+    }
 
 }
